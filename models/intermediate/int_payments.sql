aa11a33cff45d4bb96bdf056aae0bbd66543dbb3
Francesco <francesco@feedr.co>
2023-07-15 21:49:22 +0000

    Add chargeback field to intermediate layer

--- models/intermediate/int_payments.sql
+++ models/intermediate/int_payments.sql
@@ -13,6 +13,7 @@ payments_to_usd_currency as (
    select
     ref_id,
     created_at,
+    external_ref_id,
     state,
     country,
     currency,
@@ -37,40 +38,49 @@ payments_to_usd_currency as (
       {%- endfor %}
 
    from payments
-   group by 1,2,3,4,5,6,7,8,9,10,11,12
+   group by 1,2,3,4,5,6,7,8,9,10,11,12,13
 
 ),
 
 unified_data as (
-    SELECT ref_id, created_at, state, country, currency, round(usd_amount, 2) as usd_amount
-    FROM (
-        SELECT ref_id, created_at, state, country, currency, CAD_amount / cad AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'CAD'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, EUR_amount / eur AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'EUR'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, MXN_amount / mxn AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'MXN'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, USD_amount / usd AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'USD'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, SGD_amount / sgd AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'SGD'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, AUD_amount / aud AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'AUD'
-        UNION ALL
-        SELECT ref_id, created_at, state, country, currency, GBP_amount / gbp AS usd_amount
-        FROM payments_to_usd_currency
-        WHERE currency = 'GBP'
-))
- 
-select * from unified_data
+    select ref_id, created_at, external_ref_id, state, country, currency, round(usd_amount, 2) as usd_amount
+    from (
+        select ref_id, created_at, external_ref_id, state, country, currency, CAD_amount / cad AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'CAD'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, EUR_amount / eur AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'EUR'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, MXN_amount / mxn AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'MXN'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, USD_amount / usd AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'USD'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, SGD_amount / sgd AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'SGD'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, AUD_amount / aud AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'AUD'
+        union all
+        select ref_id, created_at, external_ref_id, state, country, currency, GBP_amount / gbp AS usd_amount
+        from payments_to_usd_currency
+        where currency = 'GBP'
+)),
+
+final_with_chargeback as (
+    select 
+    u.*,
+    c.chargeback 
+    from unified_data u
+    left join {{ ref('stg_chargeback') }} c
+    on c.external_ref_id = u.external_ref_id 
+)
+
+select * from final_with_chargeback
