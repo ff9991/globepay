7d65304ed8892cfe22f9ca7288bad83f8f1f00e2
Francesco <francesco@feedr.co>
2023-07-15 21:39:35 +0000

    Clean up formatting of integer values

--- models/intermediate/int_payments.sql
+++ models/intermediate/int_payments.sql
@@ -15,6 +15,7 @@ payments_to_usd_currency as (
     created_at,
     state,
     country,
+    currency,
     cad,
     eur,
     mxn,    
@@ -24,46 +25,52 @@ payments_to_usd_currency as (
     gbp,
       {% for currency in currencies -%}
  
-        sum(
+        cast(
+            sum(
             case
                when currency = '{{ currency }}' 
                then amount 
                else 0 
             end
-         ) as {{ currency }}_amount,
+         ) as FLOAT64) as {{ currency }}_amount,
 
       {%- endfor %}
-      sum(amount) as total_amount
 
    from payments
-   group by 1,2,3,4,5,6,7,8,9,10,11
+   group by 1,2,3,4,5,6,7,8,9,10,11,12
 
 ),
 
 unified_data as (
-SELECT ref_id, created_at, state, country, currency, usd_amount
-FROM (
-    SELECT ref_id, created_at, state, country, 'CAD' AS currency, cad * CAD_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'EUR' AS currency, eur * EUR_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'MXN' AS currency, mxn * MXN_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'USD' AS currency, usd * USD_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'SGD' AS currency, sgd * SGD_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'AUD' AS currency, aud * AUD_amount AS usd_amount
-    FROM payments_to_usd_currency
-    UNION ALL
-    SELECT ref_id, created_at, state, country, 'GBP' AS currency, gbp * GBP_amount AS usd_amount
-    FROM payments_to_usd_currency
+    SELECT ref_id, created_at, state, country, currency, round(usd_amount, 2) as usd_amount
+    FROM (
+        SELECT ref_id, created_at, state, country, currency, CAD_amount / cad AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'CAD'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, EUR_amount / eur AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'EUR'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, MXN_amount / mxn AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'MXN'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, USD_amount / usd AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'USD'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, SGD_amount / sgd AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'SGD'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, AUD_amount / aud AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'AUD'
+        UNION ALL
+        SELECT ref_id, created_at, state, country, currency, GBP_amount / gbp AS usd_amount
+        FROM payments_to_usd_currency
+        WHERE currency = 'GBP'
 ))
-
  
 select * from unified_data
